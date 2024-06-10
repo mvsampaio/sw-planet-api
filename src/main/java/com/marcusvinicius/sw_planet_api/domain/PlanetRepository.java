01eb46ee6e63ed854d04d7d1c60cc326a2b29356
Marcus Sampaio <sampaiosoftware@gmail.com>
2024-06-10 16:20:00 -0300

    Criado a estrutura básica da API

---
+++ src/main/java/com/marcusvinicius/sw_planet_api/domain/PlanetRepository.java
@@ -0,0 +1,6 @@
+package com.marcusvinicius.sw_planet_api.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlanetRepository extends CrudRepository<Planet, Long> {
+}
