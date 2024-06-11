092c76e0708e848a3d8ae7081b6a1f8dad07a089
Marcus Sampaio <sampaiosoftware@gmail.com>
2024-06-11 10:03:07 -0300

    Testando o cenário de sucesso no repository

---
+++ src/test/java/com/marcusvinicius/sw_planet_api/domain/PlanetRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.marcusvinicius.sw_planet_api.domain;
+
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static com.marcusvinicius.sw_planet_api.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class PlanetRepositoryTest {
+    @Autowired
+    private PlanetRepository planetRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet() {
+        Planet planet = planetRepository.save(PLANET);
+
+        Planet sut = testEntityManager.find(Planet.class, planet.getId());
+
+        //System.out.println(planet);
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getName()).isEqualTo(planet.getName());
+        assertThat(sut.getClimate()).isEqualTo(planet.getClimate());
+        assertThat(sut.getTerrain()).isEqualTo(planet.getTerrain());
+    }
+}
