cc40069178f71bf82271e122d11096d2bf502a3d
Marcus Sampaio <sampaiosoftware@gmail.com>
2024-06-12 14:45:41 -0300

    Testando o cadastro do cliente e incluso configuração de rollbak para os testes

--- src/test/java/com/marcusvinicius/sw_planet_api/PlanetIT.java
+++ src/test/java/com/marcusvinicius/sw_planet_api/PlanetIT.java
@@ -1,14 +1,33 @@
 package com.marcusvinicius.sw_planet_api;
 
+import com.marcusvinicius.sw_planet_api.domain.Planet;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import static com.marcusvinicius.sw_planet_api.common.PlanetConstants.PLANET;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = {"/remove_planets.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class PlanetIT {
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
-    public void contextLoads() {
+    public void createPlanet_ReturnCreated() {
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody().getId()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
     }
 }
